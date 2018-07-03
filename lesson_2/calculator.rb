cdd5383a809730f92a6f43444a4c151dd90a5f75
Amanda <amandagsutter@gmail.com>
2018-07-03 16:52:56 -0700

    add calculator.rb & test.rb

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -3,30 +3,114 @@
 # perform the operation on the two numbers
 # output the results
 
-Kernal.gets()
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yaml')
 
-Kernal.puts("Welcome to the Calculator!")
+LANGUAGE = 'en'
 
-Kernal.puts("What's the first number?")
-number1 = Kernal.gets().chomp()
-
-Kernal.puts("What's the second number?")
-number2 = Kernal.gets().chomp()
-
-Kernal.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernal.gets().chomp()
-
-if operator    == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-elsif operator == '4'
-  result = number1.to_f() / number2.to_f()
+def messages(message, lang='en')
+  MESSAGES[lang][message]
 end
 
-Kernal.puts("The result is #{result}.")
+def prompt_no_yaml(message)
+  Kernel.puts("=> #{message}")
+end
 
+def prompt(key)
+  message = messages(key, LANGUAGE)
+  prompt_no_yaml(message)
+end
 
+def valid_number?(num)
+  num.to_i.to_s == num
+end
 
+def number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+prompt('welcome')
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt('valid_name')
+  else
+    break
+  end
+end
+
+prompt_no_yaml("Hi, #{name}") 
+
+loop do # main loop
+  number1 = ''
+  loop do
+    prompt('number_1')
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt('invalid_number')
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt('number_2')
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt('invalid_number')
+    end
+  end
+
+  prompt('operator_prompt')
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt('operator')
+    end
+  end
+
+  prompt_no_yaml("#{operation_to_message(operator)} the two numbers...")
+
+  result =  case operator
+            when '1'
+              number1.to_i() + number2.to_i()
+            when '2'
+              number1.to_i() - number2.to_i()
+            when '3'
+              number1.to_i() * number2.to_i()
+            when '4'
+              number1.to_f() / number2.to_f()
+            end
+
+  prompt_no_yaml("The result is #{result}.")
+
+  prompt('again')
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt('goodbye')
--- lesson_2/test.rb
+++ lesson_2/test.rb
@@ -1,18 +1,51 @@
-def find_greatest(numbers)
-  saved_number = nil
+VALID_CHOICES = %w(rock paper scissors lizard spock)
 
-  numbers.each do |num|
-    saved_number ||= num  # assign to first value
-    if saved_number >= num
-      next
-    else
-      saved_number = num
-    end
-  end
+winner = {'rock'      => [VALID_CHOICES[3], 
+                          VALID_CHOICES[2]],
+          'paper'     => [VALID_CHOICES[0],
+                          VALID_CHOICES[4]],
+          'scissors'  => [VALID_CHOICES[1],
+                          VALID_CHOICES[3]],
+          'lizard'    => [VALID_CHOICES[4],
+                          VALID_CHOICES[1]],
+          'spock'     => [VALID_CHOICES[0],
+                          VALID_CHOICES[2]]
+          }
 
-  saved_number
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-num = [1, 3, 2, 5, 2.5, 5.7, 0]
+def win?(first, second, hash)
+  # second == hash[first][0] || second == hash[first][1]
+  hash[second].include?(first)
+end
 
-puts find_greatest(num)
+def display_results(player, computer, hash)
+  return prompt('You won!') if win?(computer, player, hash)
+  return prompt('Computer won!') if win?(player, computer, hash) 
+  prompt("It's a tie!")
+end
+
+loop do
+  player_choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    player_choice = Kernel.gets().chomp().downcase()
+
+    break if VALID_CHOICES.include?(player_choice)
+    prompt("That's not a valid choice.")
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  Kernel.puts("You chose: #{player_choice}; Computer chose: #{computer_choice}")
+
+  display_results(player_choice, computer_choice, winner)
+
+  prompt('Do you want to play again?')
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt('Thank you for playing. Good bye!')
